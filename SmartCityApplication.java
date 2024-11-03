1724258b433137d89fc118fe8d70bba0e73127b6
Mounika2200032262 <147919163+Mounika2200032262@users.noreply.github.com>
2024-11-03 21:32:01 +0530

    Create SmartCityApplication.java

---
+++ SmartCityApplication.java
@@ -0,0 +1,14 @@
+package com.klu.SmartCityApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartCityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartCityApplication.class, args);
+		System.out.println("Welcome to Smart city application");
+	}
+
+}
